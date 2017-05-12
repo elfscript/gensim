abb2d678c76e9393ec5dc3ed9ec99f25918f32c6
3hdeng <3hdeng@gmail.com>
2017-05-12 04:07:29 +0000

    pip install testfixtures statsmodels

--- docs/notebooks/doc2vec-imdb2.py
+++ docs/notebooks/doc2vec-imdb2.py
@@ -38,6 +38,7 @@ from collections import OrderedDict
 import multiprocessing
 
 cores = multiprocessing.cpu_count()
+print("number of cores", cores)
 assert gensim.models.doc2vec.FAST_VERSION > -1, "this will be painfully slow otherwise"
 
 simple_models = [
@@ -57,6 +58,117 @@ for model in simple_models[1:]:
     print(model)
 
 models_by_name = OrderedDict((str(model), model) for model in simple_models)
-print(models_by_name)
+
+#======== Helper methods for evaluating error rate.
+import numpy as np
+import statsmodels.api as sm
+from random import sample
+
+# for timing
+from contextlib import contextmanager
+from timeit import default_timer
+import time 
+
+@contextmanager
+def elapsed_timer():
+    start = default_timer()
+    elapser = lambda: default_timer() - start
+    yield lambda: elapser()
+    end = default_timer()
+    elapser = lambda: end-start
+    
+def logistic_predictor_from_data(train_targets, train_regressors):
+    logit = sm.Logit(train_targets, train_regressors)
+    predictor = logit.fit(disp=0)
+    #print(predictor.summary())
+    return predictor
+
+def error_rate_for_model(test_model, train_set, test_set, infer=False, infer_steps=3, infer_alpha=0.1, infer_subsample=0.1):
+    """Report error rate on test_doc sentiments, using supplied model and train_docs"""
+
+    train_targets, train_regressors = zip(*[(doc.sentiment, test_model.docvecs[doc.tags[0]]) for doc in train_set])
+    train_regressors = sm.add_constant(train_regressors)
+    predictor = logistic_predictor_from_data(train_targets, train_regressors)
+
+    test_data = test_set
+    if infer:
+        if infer_subsample < 1.0:
+            test_data = sample(test_data, int(infer_subsample * len(test_data)))
+        test_regressors = [test_model.infer_vector(doc.words, steps=infer_steps, alpha=infer_alpha) for doc in test_data]
+    else:
+        test_regressors = [test_model.docvecs[doc.tags[0]] for doc in test_docs]
+    test_regressors = sm.add_constant(test_regressors)
+    
+    # predict & evaluate
+    test_predictions = predictor.predict(test_regressors)
+    corrects = sum(np.rint(test_predictions) == [doc.sentiment for doc in test_data])
+    errors = len(test_predictions) - corrects
+    error_rate = float(errors) / len(test_predictions)
+    return (error_rate, errors, len(test_predictions), predictor)
+
+
+#======== Bulk training ==========
+#(On a 4-core 2.6Ghz Intel Core i7, these 20 passes training and evaluating 3 main models takes about an hour.)
+
+from collections import defaultdict
+best_error = defaultdict(lambda :1.0)  # to selectively-print only best errors achieved
+
+from random import shuffle
+import datetime
+
+alpha, min_alpha, passes = (0.025, 0.001, 2)
+alpha_delta = (alpha - min_alpha) / passes
+
+print("START %s" % datetime.datetime.now())
+
+for epoch in range(passes):
+    shuffle(doc_list)  # shuffling gets best results
+    
+    for name, train_model in models_by_name.items():
+        # train
+        duration = 'na'
+        train_model.alpha, train_model.min_alpha = alpha, alpha
+        with elapsed_timer() as elapsed:
+            train_model.train(doc_list, total_examples=train_model.corpus_count, epochs=train_model.iter)
+            duration = '%.1f' % elapsed()
+            
+        # evaluate
+        eval_duration = ''
+        with elapsed_timer() as eval_elapsed:
+            err, err_count, test_count, predictor = error_rate_for_model(train_model, train_docs, test_docs)
+        eval_duration = '%.1f' % eval_elapsed()
+        best_indicator = ' '
+        if err <= best_error[name]:
+            best_error[name] = err
+            best_indicator = '*' 
+        print("%s%f : %i passes : %s %ss %ss" % (best_indicator, err, epoch + 1, name, duration, eval_duration))
+
+        if ((epoch + 1) % 5) == 0 or epoch == 0:
+            eval_duration = ''
+            with elapsed_timer() as eval_elapsed:
+                infer_err, err_count, test_count, predictor = error_rate_for_model(train_model, train_docs, test_docs, infer=True)
+            eval_duration = '%.1f' % eval_elapsed()
+            best_indicator = ' '
+            if infer_err < best_error[name + '_inferred']:
+                best_error[name + '_inferred'] = infer_err
+                best_indicator = '*'
+            print("%s%f : %i passes : %s %ss %ss" % (best_indicator, infer_err, epoch + 1, name + '_inferred', duration, eval_duration))
+
+    print('completed pass %i at alpha %f' % (epoch + 1, alpha))
+    alpha -= alpha_delta
+    
+print("END %s" % str(datetime.datetime.now()))
+
+#====================
+for rate, name in sorted((rate, name) for name, rate in best_error.items()):
+    print("%f %s" % (rate, name))
+
+
+
+#=======
+#evaluate models in pairs. These wrappers return the concatenation of the vectors from each model. (Only the singular models are trained.)
+from gensim.test.test_doc2vec import ConcatenatedDoc2Vec
+models_by_name['dbow+dmm'] = ConcatenatedDoc2Vec([simple_models[1], simple_models[2]])
+models_by_name['dbow+dmc'] = ConcatenatedDoc2Vec([simple_models[1], simple_models[0]])
 
 
